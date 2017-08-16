122270183995786e2d7306d7420e2f60b06383d0
rerickson <rerickson@usgs.gov>
2017-08-16 15:52:49 -0500

    Created node model that replicates R package

--- networkNodeIPM.py
+++ networkNodeIPM.py
@@ -2,6 +2,26 @@ import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
+class densityNegExp:  
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def __call__(self, biomass):
+        out = self.a * np.exp( -1 *  self.b  * biomass)
+        return out 
+
+def densityNone(biomass):
+    return 1 
+    
+class lengthWeight:
+    def __init__(self, alphaLW, betaLW):
+        self.alphaLW = alphaLW
+        self.betaLW  = betaLW
+    def __call__(self, omega):
+        out = 10 ** (self.alphaLW +
+                     np.log10(omega) * self.betaLW)
+        return out
+
 class logestic:
     def __init__(self, alphaL, betaL, minL, maxL):
         self.alphaL = alphaL
@@ -15,49 +35,92 @@ class logestic:
               ( 1 + np.exp( self.betaL * ( np.log(z) - np.log(self.alphaL))))  
         return out
 
+class growthVB:
+    def __init__(self, aG, kG, sigmaG):
+        self.aG = aG
+        self.kG = kG
+        self.sigmaG = sigmaG
+
+    def __call__(self, zIn, zPrimeIn):
+        z = np.atleast_1d(zIn)
+        zPrime = np.atleast_1d(zPrimeIn)
+
+        out = np.zeros( ( len(zPrime), len(z) ))
+        
+        for index in range(0, len(z)):
+            out[ : , index ] = stats.norm.pdf(x = zPrime, \
+                                            loc = (1 - self.kG) * z[index] + self.kG * self.aG,
+                                            scale = self.sigmaG)
+        return(out)
+
+class linearRecruitment:
+    def __init__( self, omega, lengthWeight, survival, probabilityReproducing, eggTransition, eggPerkg, muJ, sigmaJ):
+        self.juvenile = (stats.lognorm.pdf(omega, loc = 0, s = sigmaJ, scale = muJ) / 
+                         stats.lognorm.pdf(omega, loc = 0, s = sigmaJ, scale = muJ).sum())
+        self.lengthWeight = lengthWeight
+        self.survival = survival
+        self.probabilityReproducing = probabilityReproducing
+        self.eggTransition = eggTransition
+        self.eggPerkg = eggPerkg
+        
+    def __call__(self, omega, omegaPrime):
+        omega      = np.atleast_1d(omega)
+        omegaPrime = np.atleast_1d(omegaPrime)
+        out = np.zeros( ( len(omegaPrime), len(omega) ))
+        for index in range(0, len(omega)):
+            out[ : , index ] = self.eggTransition * self.eggPerkg * self.survival(omega) * self.probabilityReproducing(omega) * \
+                               self.lengthWeight(omega) * self.juvenile
+        return(out)
+   
 class node:
-    ## I think I will want to inherit z, nYears, nSizeBins, and sizeBins from the network
+    ## I think I will want to include omega, nYears, nSizeBins, and sizeBins from the network
     ## Also, I might want to include checks to make sure the popSize0 and popLenDist0 are in the correct format
-    def __init__(self, nodeName, popSize0, popLenDist0, z, nYears):
+    def __init__(self, nodeName, popSize0, popLenDist0, omega,
+                 nYears, survival, growth, recruitment, density, lengthWeight,  emigration, immigration):
         self.nYears = nYears
         self.nodeName = nodeName
         self.popSize = np.zeros(nYears + 1)
         self.popSize[0] = popSize0
-        self.popLenDist = np.zeros( (nYears, len(z)))
-        self.popLenDist[0, :] = popLenDist0
+        self.popLenDist = np.zeros( (nYears + 1, len(omega)))
+        self.popLenDist[0, :] = popLenDist0 * popSize0
+        self.survival = survival
+        self.growth = growth
+        self.recruitment = recruitment
+        self.density = density
+        self.lengthWeight = lengthWeight 
+        self.immigration = immigration
+        self.emigration = emigration
+        self.omega = omega
+        self.hWidth = self.omega[1] - self.omega[0]      
+
+
         
+    def annualTimeStep(self, t):
+        ## Kevin, is this the best way to do this?
+        biomass = np.sum(self.popLenDist[ t, :] * self.lengthWeight(self.omega))
+        decrease = self.density(biomass)
+        
+        self.popLenDist[t + 1, : ] = ( np.dot( self.hWidth * self.growth( self.omega, self.omega),
+                                               self.survival( self.omega) * self.popLenDist[t, :]) +
+                                       np.dot( self.hWidth * self.recruitment( self.omega, self.omega),
+                                               self.popLenDist[t, :]) * decrease +    
+                                       self.immigration - self.emigration)
+        self.popSize[t + 1] =  self.popLenDist[ t + 1, :].sum()
+
+    def plotLengthTime(self):
+        fig, ax = plt.subplots()
+        for index in range(0, self.popLenDist.shape[0]):
+            ax.plot(self.omega, self.popLenDist[ index, :])      
+        plt.title("Length distributions " + self.nodeName)
+        plt.xlabel("Length")
+        plt.ylabel("Population in size class")
+        plt.show()
+
     def plotPop(self):
         plt.plot(np.arange(0, self.nYears + 1, 1),  self.popSize)
-        plt.title("Population size through time for" + self.nodeName)
+        plt.title("Population size through time for " + self.nodeName)
         plt.xlabel("Time (years)")
         plt.ylabel("Total population at node")
         plt.show()
 
-        
-        
-## Next steps: 0) Include survival, 1) include growth/with density 2) include 
-    
-survival = logestic( alphaL = 40, betaL = -5, minL = 0.1, maxL = 0.7)
-print survival(0)
-            
 
-z = np.arange( 1, 150, 1)
-sur = survival(z)
-print len(sur)
-
-
-# plt.plot(z, sur)
-# plt.xlabel("Length (cm)")
-# plt.ylabel("Survival")
-# plt.show()
-
-nYears = 10
-
-testNode = node(nodeName = "node 1", \
-                popSize0 = 1000, \
-                popLenDist0 = stats.lognorm.pdf(z, loc = 0, s = 0.2, scale = 50), \
-                z = z, \
-                nYears = nYears)
-
-
-testNode.plotPop()
---
+++ nodeDevelopmentCode.py
@@ -0,0 +1,91 @@
+import numpy as np
+import scipy.stats as stats
+import networkNodeIPM  as nnIPM
+
+## Setup numerical mesh
+minLength = 0
+maxLength = 240,
+nPoints = 200
+omega = np.linspace( start = minLength, stop = maxLength, num = nPoints +2)[1:-1]
+
+
+## Denisty parameter and distribution 
+a = 1
+b = 3e-7 # 1e-3
+
+density = nnIPM.densityNegExp(a = a, b = b)
+# density = densityNone
+
+minS = 0.1
+maxS = 0.9
+alphaS = 40 # inflection point
+betaS  =  -5 # slope
+
+survival = nnIPM.logestic( alphaL = alphaS, betaL = betaS, minL = minS, maxL = maxS)
+
+
+initMean = 150
+popLenDist0 = stats.lognorm.pdf(omega, loc = 0, s = 0.2, scale = initMean) / stats.lognorm.pdf(omega, loc = 0, s = 0.2, scale = initMean).sum()
+
+
+aG = 180
+kG = 0.16
+sigmaG = 10
+growth = nnIPM.growthVB(aG = aG, kG = kG, sigmaG = sigmaG)
+
+## the probabilty of reproducing
+minR = 0
+maxR = 1.0
+alphaR = 40
+betaR  = -4
+
+## Age 1 length dist
+muJ = np.log(10)
+sigmaJ = np.log(2)
+
+## relationship between length and weigth
+alphaLW = -4.33
+betaLW = 2.77
+
+## eggs per kg
+eggPerkg = 5e3 # default is 5e3
+eggTransition = 9e-1  # 3e-3
+
+probabilityReproducing = nnIPM.logestic( alphaL = alphaR, betaL = betaR, minL = minR, maxL = maxR)
+lengthWeightUse = nnIPM.lengthWeight( alphaLW, betaLW)
+
+recruitment = nnIPM.linearRecruitment(omega = omega,
+                                      lengthWeight = lengthWeightUse,
+                                      probabilityReproducing = probabilityReproducing,
+                                      survival = survival,
+                                      eggTransition = eggTransition, eggPerkg = eggPerkg, muJ = muJ, sigmaJ = sigmaJ)
+
+nYears = 300
+
+immigration = 0
+emigration = 0
+
+popSize0 = 8.5e3
+
+testNode = nnIPM.node(nodeName = "node 1", 
+                      popSize0 = popSize0, 
+                      popLenDist0 = popLenDist0, 
+                      omega = omega,
+                      nYears = nYears, 
+                      survival = survival, 
+                      growth = growth,
+                      recruitment = recruitment,
+                      density = density,
+                      lengthWeight = lengthWeightUse,
+                      immigration = immigration, 
+                      emigration = emigration)
+
+for year in range(0, nYears):
+    testNode.annualTimeStep(year)
+
+
+testNode.plotPop()
+testNode.plotLengthTime()
+print testNode.popLenDist.sum(1)
+print "Done" 
+
