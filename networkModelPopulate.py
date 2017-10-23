6fcea368cb231e4b5d8827564c288330a1ee378a
rerickson <rerickson@usgs.gov>
2017-10-23 12:13:43 -0500

    added demographic functions (but not projections yet

--- networkModelPopulate.py
+++ networkModelPopulate.py
@@ -1,31 +1,110 @@
 import networkModel as nm
+import numpy as np
+import scipy.stats as stats
 
-class populatedPath( nm.path):
+class populatedHelpers:
+    ''' Helper funcitons that are coded in one place to avoid repeating'''
+    
+    def addGroups(self, groups):
+        self.groups = groups
 
+    def showGroups(self):
+        return self.groups
+
+class logistic:
+    '''Defines a logistic function.''' 
+    def __init__(self, alphaL, betaL, minL, maxL):
+        self.alphaL = alphaL
+        self.betaL = betaL
+        self.minL = minL
+        self.maxL = maxL       
+
+    def __call__(self, z):
+        out = ( self.minL + 
+                (self.maxL - self.minL) / 
+                ( 1 + np.exp( self.betaL * ( np.log(z) - np.log(self.alphaL)))) )
+        return out
+
+   
+class densityNegExp:
+    '''Define a function where density has a negative exponential impact on the system.'''
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def __call__(self, biomass):
+        out = self.a * np.exp( -1 *  self.b  * biomass)
+        return out 
+    
+class lengthWeight:
+    '''Define a relationship between length and weight.'''
+    def __init__(self, alphaLW, betaLW):
+        self.alphaLW = alphaLW
+        self.betaLW  = betaLW
+    def __call__(self, omega):
+        out = 10 ** (self.alphaLW +
+                     np.log10(omega) * self.betaLW)
+        return out
+
+class growthVB:
+    '''
+    Define a von Bertalanffy growth function that maps the length at a 
+    current year to the probability of length at a future year. 
+    '''
+    def __init__(self, aG, kG, sigmaG):
+        self.aG = aG
+        self.kG = kG
+        self.sigmaG = sigmaG
+
+    def __call__(self, zIn, zPrimeIn):
+        z = np.atleast_1d(zIn)
+        zPrime = np.atleast_1d(zPrimeIn)
+
+        out = np.zeros( ( len(zPrime), len(z) ))
+        
+        for index in range(0, len(z)):
+            out[ : , index ] = stats.norm.pdf(x = zPrime, 
+                                            loc = (1 - self.kG) * z[index] + self.kG * self.aG,
+                                            scale = self.sigmaG)
+        return(out)
+
+    
+class populatedPath( nm.path, populatedHelpers):
     def __init__(self, pathName):
         self.pathName = pathName
         self.groups = []
         self.startNode = ''
         self.endNode = ''
 
-    def addGroups(self, groups):
-        self.groups = groups
 
-    def showGroups(self):
-        return self.groups
-
-
-
-class populatedNode( nm.node):
+class populatedNode( nm.node, populatedHelpers):
     def __init__(self, nodeName):
         self.nodeName = nodeName
         self.groups = []
         self.pathsIn = []
         self.pathsOut = []
-        ## NEED TO ADD PATHS OUT PROBABIITY 
-    def addGroups(self, groups):
-        self.groups = groups
 
-    def showGroups(self):
-        return self.groups
+class group:
+    def __init__(self, groupName):
+        self.groupName = groupName
+        
+    def showGroupName(self):
+        return self.groupName
+
+    def createPopDist(self, nYears, nPoints, popDist0 = None):
+        if popDist0 is None:
+            self.popDist0 = np.zeros(nPoints)
+        else:
+            self.popDist0 = popDist0
+            
+        self.popDist = np.zeros( (nYears, nPoints))
+        self.popDist[ 0, :] = self.popDist0
+
+    def showPopDist(self):
+        return self.popDist
+
+    def showPopDistYear(self, year):
+        return self.popDist[ year, :]
+    
+    def updatePopDistYear(self, year, popAdd):
+        self.popDist[ year, :] += popAdd
 
--- unitTestNetworkPopulate.py
+++ unitTestNetworkPopulate.py
@@ -56,6 +56,73 @@ class test_populatedNode( unittest.TestCase):
         self.populatedNode.addGroups(['male', 'female'])
         
         self.assertEqual( self.populatedNode.showGroups(), ['male', 'female'])
+
+class test_gropu( unittest.TestCase):
+
+    def test_group(self):
+        self.group = nmp.group( "test group")
+        self.assertEqual( self.group.showGroupName(), "test group")
+
+    def test_createPopDist(self):
+        ''' test createPopDist and showPopDist''' 
+        self.group = nmp.group( "test group")
+        self.group.createPopDist( 10, 24)
+        self.assertEqual( self.group.showPopDist().shape, (10, 24))
+        self.assertEqual( self.group.showPopDist().sum(1)[0], 0 )
+
+        self.group = nmp.group( "test group 2")
+        self.group.createPopDist( 10, 24, np.ones(24))
+        self.assertEqual( self.group.showPopDist().sum(1)[0], 24)
+
+    def test_yearUpdateAndYearShow(self):
+        ''' test createPopDist and showPopDist''' 
+        self.group = nmp.group( "test group")
+        self.group.createPopDist( 2, 24, np.ones(24))
+        self.group.updatePopDistYear(1, self.group.showPopDistYear(0) * 2.0)
+        self.group.showPopDistYear(1)
+        self.assertEqual( self.group.showPopDistYear(1).sum(), 48)
+        
+class test_logistic(unittest.TestCase):
+    
+    def test_one(self):
+        self.logistic = nmp.logistic( 40.0, -5.0, 0.0, 1.0)
+        self.assertEqual( self.logistic(2e8), 1.0)
+
+    def test_inflecitonPoint(self):
+        self.logistic = nmp.logistic( 40.0, -5.0, 0.0, 1.0)
+        self.assertEqual( self.logistic( 40.0), 0.5)
+
+
+class test_densityNegExp(unittest.TestCase):
+
+    def test_zeros(self):
+        """Constructor"""
+        a = 10.0
+        b = 11.0
+        biomass = 0.0
+        zeroFunc = nmp.densityNegExp(a = a, b = b)
+
+        self.assertEqual( zeroFunc(biomass = biomass), 10.0)
+
+class test_lengthWeight(unittest.TestCase):
+
+    def test_zero(self):
+        self.lengthWeight = nmp.lengthWeight(alphaLW = 1.0,
+                                               betaLW  = 2.0)
+        self.assertEqual( self.lengthWeight(1.0), 10.0)
+
+    def test_nonzero(self):
+        self.lengthWeight = nmp.lengthWeight(alphaLW = 1.0,
+                                               betaLW  = 2.0)
+        self.assertEqual( self.lengthWeight(10.0), 1000.0)
+
+        
+class test_growthVB(unittest.TestCase):
+
+    def test_growthVB(self):
+        self.growthVB = nmp.growthVB( 180.0, 0.16, 10.0)
+        self.assertEqual( self.growthVB(180, 180), 0.039894228040143268)
+
         
 if __name__ == '__main__':
     unittest.main()
